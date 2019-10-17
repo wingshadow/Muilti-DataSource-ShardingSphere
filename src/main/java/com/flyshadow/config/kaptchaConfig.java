a3f232dcc01d709d08ef22d39b4d04ddbdde2dc3
wingshadow <zhangqiyi1@163.com>
2019-10-17 09:16:37 +0800

    增加验证码组件，验证码配置KaptchaConfig.java

---
+++ src/main/java/com/flyshadow/config/kaptchaConfig.java
@@ -0,0 +1,30 @@
+package com.flyshadow.config;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * @Title: kaptchaConfig
+ * @ProjectName sharding-master-slave
+ * @Author FlyShadow
+ * @Date 2019/10/17 9:01
+ */
+@Configuration
+public class kaptchaConfig {
+
+    @Bean
+    public DefaultKaptcha producer() {
+        Properties properties = new Properties();
+        properties.put("kaptcha.border", "no");
+        properties.put("kaptcha.textproducer.font.color", "black");
+        properties.put("kaptcha.textproducer.char.space", "5");
+        Config config = new Config(properties);
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
+    }
+}
